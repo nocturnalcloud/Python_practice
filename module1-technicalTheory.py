901362786b0a88401f537bc6ac060947ffdf17c8
Noctunal Cloud <nocturncloud.dev@gmail.com>
2023-07-26 19:58:36 +0000

    Learned about technical theory with python

---
+++ module1-technicalTheory.py
@@ -0,0 +1,30 @@
+#what is a computer program?
+# computer program = a collection of instructions executed by a computer
+
+# each computer has an instruction list
+# instruction list = the set of instructions the given computer can execute; written in the form of machine code
+
+# we use high level to create source code
+
+# source code ----------> machine code
+
+# this transofrmation can be done in 2 ways!
+
+# 1. compilation
+#  - compile the source code only once
+#  - anyone can run the executable
+
+# for exqmple when you have microsoft windows it gives you an exe file and you can share with who ever
+
+# 2.interpretation
+#  - interpret source code everytime you run programs
+#. - everyone needs the interpreter to run
+
+# python is an inrepter alnguage or scripting language
+
+# Pythong typically reads code from top to bottom
+# throws errors based on checking:
+#. 1. lexis --------> keywords
+#. 2. syntax -------> rules for constructing python instructions
+#. 3. semantics -----> additional prompts
+
