f287cdadf56f85e6f7f365c0b8acfd9f7bdb4479
Noctunal Cloud <nocturncloud.dev@gmail.com>
2023-07-26 00:02:59 +0000

    talked about strings, concatanation and different reassignment values

---
+++ module1-reqassigning-values.py
@@ -0,0 +1,29 @@
+age = 28
+print(age)
+
+age = 28
+new_age = age + 5
+print(new_age)
+
+age = 28
+age = age + 7
+print(age)
+
+age = 28
+age += 7
+print(age)
+
+
+# current value times 2
+age *= 2
+# current value minus 5
+age -= 5
+# current value divided by 3
+age /= 3
+
+#string concatenation there is no space from output
+text = 'hokus' + 'pokus'
+print(text)
+
+#prints text 5 times
+print('hokus' * 5)
