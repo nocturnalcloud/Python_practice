cee9d7f46aad2e15ccd0c236d61267c1a3d1ad32
Noctunal Cloud <nocturncloud.dev@gmail.com>
2023-07-28 00:40:33 +0000

    Created a salary calculator

---
+++ Salary_calculator.py
@@ -0,0 +1,8 @@
+
+# salary calculator: 
+hours = float(input('How many hours did you work last month? '))
+hourly_rate = float(input('What is your hourly rate? '))
+
+salary = hours * hourly_rate
+
+print('Last month, you earned', salary, 'dollars')
